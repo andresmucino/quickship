6bd9834f40dc1115566e9ec0cf101a1642cbbabd
andres <mucino.andres1990@gmail.com>
2023-04-25 17:29:09 -0600

    fix dependencies and process

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,14 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   const port = process.env.APLICATION_PORT || 3000;
 
   await app.listen(port).then(() => {
     console.log(`Server is running at http://localhost:${port}/graphql`);
   });
-  
+ 
 }
 bootstrap();
