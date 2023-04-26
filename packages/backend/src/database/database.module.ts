c79883d5e5e70330ae56967aaee7452f80fa680f
andres <mucino.andres1990@gmail.com>
2023-04-25 21:18:40 -0600

    add database

---
+++ packages/backend/src/database/database.module.ts
@@ -0,0 +1,32 @@
+import { Global, Module } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from 'src/config';
+
+@Global()
+@Module({
+    imports: [
+        TypeOrmModule.forRootAsync({
+            inject: [config.KEY],
+            useFactory: (configService: ConfigType<typeof config>) => {
+              const { database, host, password, port, url, username } = configService;
+      
+              return {
+                type: 'mysql',
+                host: 'localhost',
+                port: 3306,
+                // url: url,
+                username: 'root',
+                password: '123456',
+                database: 'mydatabase',
+                autoLoadEntities: true,
+                synchronize: true,
+                // ssl: {
+                //   rejectUnauthorized: false,
+                // },
+              };
+            },
+        })
+    ]
+})
+export class DatabaseModule {}
